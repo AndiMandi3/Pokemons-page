ee9d658c03ea1dbd3f7eeb633a6a74c9e0b98d6e
Димаков Андрей Владиславович <Dimakov_AV@sever.local>
2025-07-21 17:59:24 +0700

    get pokemons with secure

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import './style.scss'
 import type {TPokemonType, TPokemonPreview} from './types.ts'
 
 const URL_API:string = "https://pokeapi.co/api/v2/"
-const resultDiv: HTMLDivElement | null = document.querySelector('.result__row')
 
 async function getPokemon(pokemonName: string) {
     try {
@@ -44,7 +43,7 @@ async function getPokemon(pokemonName: string) {
         return metaData
     }
     catch(error) {
-        return {}
+        return null
     }
 }
 
@@ -52,6 +51,7 @@ async function getAllPokemons(offset: number = 0) {
     try {
         const json = await fetch(URL_API + `pokemon?limit=12&offset=${offset}`)
         const data = await json.json()
+
         const listPokemons: TPokemonPreview[] = data.results
             .map(async (element: unknown) => {
                 if(!element || !(typeof element === 'object') || !('name' in element) || !(typeof element.name === 'string')) return null
@@ -68,61 +68,66 @@ async function getAllPokemons(offset: number = 0) {
 
 
 async function renderPokemons() {
-    
+    const resultDiv: HTMLDivElement | null = document.querySelector('.result__row')
     let pokemons: any = await getAllPokemons(globalOffset)
-    pokemons.map(async (element: TPokemonPreview) => {
-        const pokemonData = await element
+    if(pokemons.length > 0) {
+        for await (const element of pokemons as TPokemonPreview[]) { //tg or type as here?
 
-        const item: HTMLDivElement = document.createElement('div')
-        item.classList.add('result__item')
+            const pokemonData = element
+            
+            const item: HTMLDivElement = document.createElement('div')
+            item.classList.add('result__item')
 
-        const linkPokemon: HTMLAnchorElement = document.createElement('a')
-        linkPokemon.classList.add('result__link')
-        linkPokemon.href = "#"
+            const linkPokemon: HTMLAnchorElement = document.createElement('a')
+            linkPokemon.classList.add('result__link')
+            linkPokemon.href = "#"
 
-        const imgPokemon: HTMLImageElement = document.createElement('img')
-        imgPokemon.classList.add('result__img')
-        imgPokemon.src = pokemonData.img
-        linkPokemon.appendChild(imgPokemon)
-        item.appendChild(linkPokemon)
+            const imgPokemon: HTMLImageElement = document.createElement('img')
+            imgPokemon.classList.add('result__img')
+            imgPokemon.src = pokemonData.img
+            linkPokemon.appendChild(imgPokemon)
+            item.appendChild(linkPokemon)
 
-        const pokemonPreview: HTMLDivElement = document.createElement('div')
-        pokemonPreview.classList.add('item__pokemon-info')
+            const pokemonPreview: HTMLDivElement = document.createElement('div')
+            pokemonPreview.classList.add('item__pokemon-info')
 
-        const pokemonId: HTMLParagraphElement = document.createElement('p')
-        pokemonId.classList.add('pokemon__id')
-        pokemonId.textContent = (pokemonData.id/1000).toFixed(3).replace('.', '')
+            const pokemonId: HTMLParagraphElement = document.createElement('p')
+            pokemonId.classList.add('pokemon__id')
+            pokemonId.textContent = (pokemonData.id/1000).toFixed(3).replace('.', '')
 
-        const idPrefix: HTMLSpanElement = document.createElement('span')
-        idPrefix.classList.add('number-prefix')
-        idPrefix.textContent = "# "
+            const idPrefix: HTMLSpanElement = document.createElement('span')
+            idPrefix.classList.add('number-prefix')
+            idPrefix.textContent = "# "
 
-        pokemonId.prepend(idPrefix)
-        pokemonPreview.appendChild(pokemonId)
+            pokemonId.prepend(idPrefix)
+            pokemonPreview.appendChild(pokemonId)
+            const pokemonName: HTMLHeadingElement = document.createElement('h5')
+            pokemonName.textContent = pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)
+            pokemonPreview.appendChild(pokemonName)
+            
+            const divAbilities: HTMLDivElement = document.createElement('div')
+            divAbilities.classList.add('pokemon__abilities')
 
-        const pokemonName: HTMLHeadingElement = document.createElement('h5')
-        pokemonName.textContent = pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)
-        pokemonPreview.appendChild(pokemonName)
-
-        const divAbilities: HTMLDivElement = document.createElement('div')
-        divAbilities.classList.add('pokemon__abilities')
-
-        if(pokemonData.types.length > 0) {
-            pokemonData.types.map(async (element: string, index: number) => {
-                const pokemonAbilitie: HTMLSpanElement = document.createElement('span')
-                pokemonAbilitie.classList.add(`pokemon__abilitie--${index}`)
-                pokemonAbilitie.textContent = element.charAt(0).toUpperCase() + element.slice(1)
-                divAbilities.appendChild(pokemonAbilitie)
-            })
+            if(pokemonData.types.length > 0) {
+                pokemonData.types.map(async (element: string, index: number) => {
+                    const pokemonAbilitie: HTMLSpanElement = document.createElement('span')
+                    pokemonAbilitie.classList.add(`pokemon__abilitie--${index}`)
+                    pokemonAbilitie.textContent = element.charAt(0).toUpperCase() + element.slice(1)
+                    divAbilities.appendChild(pokemonAbilitie)
+                })
+            }
+            else {
+                return
+            }
+            
+            pokemonPreview.appendChild(divAbilities)
+            item.appendChild(pokemonPreview)
+            resultDiv?.appendChild(item)
         }
-        else {
-            return
-        }
-        
-        pokemonPreview.appendChild(divAbilities)
-        item.appendChild(pokemonPreview)
-        resultDiv?.appendChild(item)
-    })
+    }
+    else {
+        resultDiv!.innerHTML = "No pokemons found"
+    }
 }
 
 const loadMoreButton = document.querySelector('#loadMore')
